5340b89b8c0c8db201cfe48ac8810806137b75bb
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2024-04-08 12:26:09 -0400

    Put item view into its own file.

--- To-Do-ListStatic/Views/ItemView.swift
+++ To-Do-ListStatic/Views/ItemView.swift
@@ -6,3 +6,24 @@
 //
 
 import SwiftUI
+struct ItemView: View {
+    
+    let title: String
+    let done: Bool
+    
+    
+    var body: some View {
+        Label(
+            title: {
+                Text(title)
+            }, icon: {
+                Image(systemName: done == true ? "checkmark.circle" : "circle")
+            }
+            
+        )
+    }
+}
+
+#Preview {
+    ItemView(title: "have a nap", done: false)
+}
--- To-Do-ListStatic/Views/LandingView.swift
+++ To-Do-ListStatic/Views/LandingView.swift
@@ -49,27 +49,3 @@ struct LandingView: View {
     LandingView()
 }
 
-struct ItemView: View {
-    
-    let title: String
-    let done: Bool
-    
-    
-    var body: some View {
-        Label(
-            title: {
-                Text(title)
-            }, icon: {
-                Image(systemName: done == true ? "checkmark.circle" : "circle")
-//                if done == true {
-//                    Image(systemName: "checkmark.circle")
-//                } else {
-//                    Image(systemName: "circle")
-//                }
-                
-                
-            }
-            
-        )
-    }
-}
